6b7ac1b9346918c9d09811431abec519ea8c2321
114886 <1148864234@qq.com>
2022-02-06 12:37:20 +0800

    修改细节

--- src/components/Detailed.vue
+++ src/components/Detailed.vue
@@ -12,7 +12,7 @@
     >
       <el-table-column prop="createdAt" label="日期" sortable width="180">
       </el-table-column>
-      <el-table-column prop="times" label="时长(s)" sortable width="180">
+      <el-table-column prop="times" label="时长" sortable width="180">
       </el-table-column>
       <el-table-column prop="notes" label="学习成果" :formatter="formatter">
       </el-table-column>
@@ -67,6 +67,23 @@ export default {
           "YYYY年MM月DD日"
         );
       }
+      for (let i = 0; i < this.tableData.length; i++) {
+        this.tableData[i].times = this.formatTime(this.tableData[i].times);
+      }
+    },
+    formatTime(seconds) {
+      let hour =
+        Math.floor(seconds / 3600) >= 10
+          ? Math.floor(seconds / 3600)
+          : "0" + Math.floor(seconds / 3600);
+      seconds -= 3600 * hour;
+      let min =
+        Math.floor(seconds / 60) >= 10
+          ? Math.floor(seconds / 60)
+          : "0" + Math.floor(seconds / 60);
+      seconds -= 60 * min;
+      let sec = seconds >= 10 ? seconds : "0" + seconds;
+      return hour + ":" + min + ":" + sec;
     },
     addTime() {
       this.$prompt("请输入未记录的时长(单位:s)", "提示", {
